56153c897dee8c6f09422f82fdf623a94cf8233d
26june <gj.abordo1@gmail.com>
2023-11-23 11:16:08 +0000

    create single post

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,13 +1,18 @@
-export default function Page() {
+import { getPostById } from "@/app/lib/posts";
+import { log } from "console";
+
+type idParam = { params: { slug: string } };
+
+export default function Page({ params }: idParam) {
+  const singlePost = getPostById(+params.slug);
+
+  const { title, author, content } = singlePost[0];
+
   return (
     <div>
-      <h1>Title</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore cumque
-        dignissimos enim reprehenderit mollitia. Necessitatibus commodi illum
-        magni cupiditate provident dolor tenetur ea, quam in sint accusamus
-        reprehenderit incidunt at.
-      </p>
+      <h1>{title}</h1>
+      <h2>By: {author}</h2>
+      <p>{content}</p>
     </div>
   );
 }
