a1c5b2f216a5af999ddef3f14ee4ce87c8b79883
26june <gj.abordo1@gmail.com>
2023-11-23 11:16:15 +0000

    create root post

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,21 @@
+import Link from "next/link";
+import { getPosts } from "../lib/posts";
+
 export default function Page() {
-  return <div>page</div>;
+  const posts = getPosts();
+
+  return (
+    <main className="h-auto pt-5 flex flex-col justify-center items-center gap-5">
+      {posts.map((singlePost) => {
+        return (
+          <div className="h-[10vh] w-[20vw] flex items-center gap-5">
+            <Link href={`/blog/${singlePost.id}`} key={singlePost.id}>
+              {singlePost.title}
+            </Link>
+            -&gt;
+          </div>
+        );
+      })}
+    </main>
+  );
 }
