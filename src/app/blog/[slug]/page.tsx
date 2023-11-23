1dc90f04b305ff0a842a530e6ce7e9a2258f6472
26june <gj.abordo1@gmail.com>
2023-11-23 09:36:28 +0000

    add pages

---
+++ src/app/blog/[slug]/page.tsx
@@ -0,0 +1,13 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>Title</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore cumque
+        dignissimos enim reprehenderit mollitia. Necessitatibus commodi illum
+        magni cupiditate provident dolor tenetur ea, quam in sint accusamus
+        reprehenderit incidunt at.
+      </p>
+    </div>
+  );
+}
